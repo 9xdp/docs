99a64b04746c478fc255b19b3d3ae77f581facb5
EKShin <eks012@gmail.com>
2018-05-23 01:47:53 +0900

    INIT docs

---
+++ .vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "Hello VuePress",
+  description: "Just playing around",
+  serviceWorker: true,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: 'Group 1',
+        collapsable: false,
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ /* ... */]
+      }
+    ]
+  }
+}
